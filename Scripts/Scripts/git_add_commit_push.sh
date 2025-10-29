5c2bb8797ab1341d2fc47a80f49dd3f657992048
nighreport <mihaljevic.gregor@gmail.com>
2025-10-29 17:44:48 +0100

    add logging

--- Scripts/Scripts/git_add_commit_push.sh
+++ Scripts/Scripts/git_add_commit_push.sh
@@ -2,23 +2,29 @@
 
 if ! git status &> /dev/null
 then
-	echo "Not a git directory"
+	echo "Not a git directory" >&2
 	exit 1
 fi
 
 command echo "Suggested:"
-git status | rg "(modified:|deleted:)"
+git status | rg "(modified:|deleted:)" | cut -d: -f2
 command echo -n "To add: "
-read -r file_to_add
+read -er file_to_add
+
+if [[ -z $file_to_add ]]
+then
+	echo "Nothing selected. Exiting..." >&2
+	exit 1
+fi
 
 git add "$file_to_add"
-command echo "git add $file_to_add"
+command echo "[LOG ] git add $file_to_add"
 
 command echo -n "Commit message: "
 read -r commit_message
 
 git commit -m "$commit_message"
-command echo "git commit -m $commit_message"
+command echo "[LOG ] git commit -m $commit_message"
 
 command echo -n "Branch to push (default: main): "
 read -r branch_name
@@ -29,3 +35,5 @@ then
 fi
 
 git push origin "$branch_name"
+command echo "[LOG ] git push origin $branch_name"
+command echo "[ OK ] All done."
