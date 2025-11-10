4b3c668e40eea23ce506cff0878cf2c997bfb696
nighreport <mihaljevic.gregor@gmail.com>
2025-11-10 17:22:56 +0100

    add logging

--- Scripts/Scripts/set-wallpaper.sh
+++ Scripts/Scripts/set-wallpaper.sh
@@ -6,12 +6,20 @@ change_image(){
 	hyprctl hyprpaper preload ~/Pictures/"$image_name" > /dev/null
 	hyprctl hyprpaper wallpaper , ~/Pictures/"$image_name" > /dev/null
 
+	command echo "[LOG ] hyprctl hyprpaper preload ~/Pictures/""$image_name"
+	command echo "[LOG ] hyprctl hyprpaper wallpaper , ~/Pictures/""$image_name"
+
 	cd ~/.config/hypr || exit
 	rp=$(realpath ~/Pictures/"$image_name")
 	echo "preload = $rp" > hyprpaper.conf
 	echo "wallpaper = , $rp" >>  hyprpaper.conf
-	hyprctl reload
-	echo "Wallpaper changed to $image_name"
+	hyprctl reload > /dev/null
+	echo "[LOG ] Wallpaper changed to $image_name"
+	command echo -n "[ "
+	tput setaf 2
+	command echo -n "OK"
+	tput setaf 7
+	command echo " ] All done."
 }
 
 if [ $# -eq 0 ]; then
@@ -19,6 +27,7 @@ if [ $# -eq 0 ]; then
 	echo "Available images: "
 	lsd ~/Pictures/ --hyperlink=always	
 	cd ~/Pictures/ || exit 1
+	command echo -ne "Enter image name: "
 	read -er image_name
 fi
 if [ ! -f ~/Pictures/"$image_name" ]; then
