00f27d5fcaa5d34ac60448ab0bdd92bfff5856fb
nighreport <mihaljevic.gregor@gmail.com>
2026-01-13 13:52:46 +0100

    Delete file for recording name after done with usage.

--- Scripts/Scripts/Record.sh
+++ Scripts/Scripts/Record.sh
@@ -3,11 +3,12 @@
 if pgrep -x wf-recorder
 then
 	pkill -INT wf-recorder
-	filename=`cat ~/Scripts/.cur_recording`
+	filename=$(cat ~/Scripts/.cur_recording)
+	rm .cur_recording
 	notify-send "Recording ended" "Saved at ~/Videos/${filename}"
 	exit
 fi
-filename=`date +%Y_%m_%d_%H_%M_%S.mkv`
-echo $filename > ~/Scripts/.cur_recording
+filename=$(date +%Y_%m_%d_%H_%M_%S.mkv)
+echo "$filename" > ~/Scripts/.cur_recording
 notify-send "Recording started" "$filename"
-wf-recorder -a -f ~/Videos/${filename}
+wf-recorder -a -f ~/Videos/"${filename}"
